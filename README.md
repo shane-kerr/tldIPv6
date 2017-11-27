6c3f15a0356cfcc786e5265980d748d0cc0f6215
Shane Kerr <shane@time-travellers.org>
2017-11-27 11:24:40 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # tldIPv6
-Scan the DNS top-level domains and see which support IPv6
+Scan the DNS top-level domains and see which support IPv6.
+
+In order for an IPv6-only resolver to work, the authority servers that
+it queries must also support IPv6. Since DNS is hierarchical, all of
+the zones above a given domain must also support IPv6. The root DNS
+servers have had IPv6 support for many years now, but there are still
+some TLD servers which do not have any IPv6 servers. This program
+finds them.
+
+## Installation
+
+Most importantly you need a Unix machine with working IPv6.
+
+You need Python 3 and dnspython. You can install dnspython via:
+
+```
+$ pip install -r requirements.txt
+```
+
+If you do not want to install dnspython system-wide you can also do it
+only for your current user:
+
+```
+$ pip install --user -r requirements.txt
+```
+
+You can also use a virtual environment (my preferred method):
+
+```
+$ python3 -m venv venv
+$ . venv/bin/activate
+(venv) $ pip install -r requirements.txt
+```
+
+## Running
+
+It takes quite a while to run, since every lookup is done
+sequentially. The program outputs progress to the console via
+`stderr`, so you can know that it is not just stuck.
+
+Typical execution looks like this:
+
+```
+$ python3 tldIPv6.py > results-2017-11-27
+No answer or RRset not for qname; Unable to XFR from 2001:500:2d::d
+No answer or RRset not for qname; Unable to XFR from 198.41.0.4
+No answer or RRset not for qname; Unable to XFR from 199.7.83.42
+No answer or RRset not for qname; Unable to XFR from 198.97.190.53
+[  48/1541   3%] alsace
+```
+
+As you see here, there are a few errors as it tries to AXFR the root
+zone from a root server or two that does not support AXFR, but
+eventually it will work.
+
+You will get a result file that looks something like this:
+
+```
+ai  no IPv6 nameservers
+bb  no IPv6 nameservers
+bf  no IPv6 nameservers
+bh  no IPv6 nameservers
+ck  no IPv6 nameservers
+dj  no IPv6 nameservers
+fk  no IPv6 nameservers
+ .
+ .
+ .
+```
+
+## How It Works
+
+The program downloads the current root zone via an AXFR from one of
+the root servers that allows zone transfers. It does not keep a list
+of which servers support this, but just tries each root server IP
+address until the transfer works. The order attempted is whatever the
+local resolver returns for the NS for root.
+
+Next it goes through each TLD and does an NS lookup to get the
+authoritative list of name servers. It then tries each name server
+returned until it finds one with an AAAA record. If none of the name
+servers for a TLD have an AAAA record, then it is reported as not
+supporting IPv6.
---
+++ tldIPv6.py
@@ -0,0 +1,99 @@
+import sys
+
+import dns.exception
+import dns.message
+import dns.query
+import dns.rdatatype
+import dns.resolver
+
+def get_root_server_ips():
+    """get the root servers' IP addresses"""
+    # look up the root servers
+    root_ns_rr = dns.resolver.query('root-servers.net', dns.rdatatype.NS)
+    root_server_names = set()
+    for rr in root_ns_rr:
+        if rr.rdtype == dns.rdatatype.NS:
+            root_server_names.add(rr.target.to_text().lower())
+    # get the IP addresses for each root server
+    root_server_ips = set()
+    for root_server_name in root_server_names:
+        ipv4_rr = dns.resolver.query(root_server_name, dns.rdatatype.A)
+        for rr in ipv4_rr:
+            if rr.rdtype == dns.rdatatype.A:
+                root_server_ips.add(rr.address)
+        ipv6_rr = dns.resolver.query(root_server_name, dns.rdatatype.AAAA)
+        for rr in ipv6_rr:
+            if rr.rdtype == dns.rdatatype.AAAA:
+                root_server_ips.add(rr.address.lower())
+    # return our result
+    return root_server_ips
+
+def get_tlds(root_server_ips):
+    """get a list of all TLD's from the root servers"""
+    for target_ip in root_server_ips:
+        try:
+            root_xfr = dns.query.xfr(target_ip, '.')
+            root_zones = set()
+            for root_msg in root_xfr:
+                for rrset in root_msg.answer:
+                    if rrset.rdtype == dns.rdatatype.NS:
+                        name = rrset.name.to_text().lower()
+                        # filter out NS for the root zone itself
+                        if name not in ('@', '.'):
+                            root_zones.add(name)
+            return root_zones
+        except OSError as ex:
+            print("%s; Unable to XFR from %s" % (ex, target_ip),
+                  file=sys.stderr)
+        except dns.exception.FormError as ex:
+            print("%s; Unable to XFR from %s" % (ex, target_ip),
+                  file=sys.stderr)
+
+
+def ipv6_ns_check(domain):
+    """check if a domain has one or more IPv6 name servers"""
+    # make sure we are looking up absolute names
+    if not domain.endswith('.'):
+        domain = domain + '.'
+    # get our list of name servers for the domain
+    try:
+        ns_rr = dns.resolver.query(domain, dns.rdatatype.NS)
+    except dns.exception.Timeout:
+        return "lookup error: timeout"
+    except dns.resolver.NoNameservers:
+        return "lookup error: no working name servers"
+    ns_names = set()
+    for rr in ns_rr:
+        if rr.rdtype == dns.rdatatype.NS:
+            ns_names.add(rr.target.to_text().lower())
+    # see if we can get an AAAA record for one of them
+    for ns_name in ns_names:
+        try:
+            ipv6_rr = dns.resolver.query(ns_name, dns.rdatatype.AAAA)
+            for rr in ipv6_rr:
+                if rr.rdtype == dns.rdatatype.AAAA:
+                    return None
+        except dns.resolver.NoAnswer:
+            pass
+        except dns.resolver.NXDOMAIN:
+            pass
+    # if not, we have no IPv6
+    return "no IPv6 nameservers"
+
+def main():
+    root_server_ips = get_root_server_ips()
+    tlds = get_tlds(root_server_ips)
+    last_tld_len = 0
+    for n, tld in enumerate(sorted(list(tlds))):
+        progress = "[%4d/%4d %3d%%] " % (n+1, len(tlds), ((n+1)*100)/len(tlds))
+        overwrite = "\r" + "".ljust(last_tld_len+len(progress)) + "\r"
+        print(overwrite, end='', file=sys.stderr)
+        print("%s%s" % (progress, tld), end='', flush=True, file=sys.stderr)
+        err = ipv6_ns_check(tld)
+        if err:
+            print("%s\t%s" % (tld, err), flush=True)
+        last_tld_len = len(tld)
+    print(file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
