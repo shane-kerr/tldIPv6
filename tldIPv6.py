0ba2abb5a9ac98e6d0295bee96228f5b07172c72
Shane Kerr <shane@time-travellers.org>
2021-07-30 11:04:15 +0200

    Minor fix-ups

--- tldIPv6.py
+++ tldIPv6.py
@@ -9,7 +9,7 @@ import dns.resolver
 def get_root_server_ips():
     """get the root servers' IP addresses"""
     # look up the root servers
-    root_ns_rr = dns.resolver.query('root-servers.net', dns.rdatatype.NS)
+    root_ns_rr = dns.resolver.resolve('root-servers.net', dns.rdatatype.NS)
     root_server_names = set()
     for rr in root_ns_rr:
         if rr.rdtype == dns.rdatatype.NS:
@@ -17,11 +17,11 @@ def get_root_server_ips():
     # get the IP addresses for each root server
     root_server_ips = set()
     for root_server_name in root_server_names:
-        ipv4_rr = dns.resolver.query(root_server_name, dns.rdatatype.A)
+        ipv4_rr = dns.resolver.resolve(root_server_name, dns.rdatatype.A)
         for rr in ipv4_rr:
             if rr.rdtype == dns.rdatatype.A:
                 root_server_ips.add(rr.address)
-        ipv6_rr = dns.resolver.query(root_server_name, dns.rdatatype.AAAA)
+        ipv6_rr = dns.resolver.resolve(root_server_name, dns.rdatatype.AAAA)
         for rr in ipv6_rr:
             if rr.rdtype == dns.rdatatype.AAAA:
                 root_server_ips.add(rr.address.lower())
@@ -48,6 +48,9 @@ def get_tlds(root_server_ips):
         except dns.exception.FormError as ex:
             print("%s; Unable to XFR from %s" % (ex, target_ip),
                   file=sys.stderr)
+        except dns.xfr.TransferError:
+            print("%s; Unable to XFR from %s" % (ex, target_ip),
+                  file=sys.stderr)
 
 
 def ipv6_ns_check(domain):
@@ -57,26 +60,36 @@ def ipv6_ns_check(domain):
         domain = domain + '.'
     # get our list of name servers for the domain
     try:
-        ns_rr = dns.resolver.query(domain, dns.rdatatype.NS)
+        ns_rr = dns.resolver.resolve(domain, dns.rdatatype.NS)
     except dns.exception.Timeout:
         return "lookup error: timeout"
     except dns.resolver.NoNameservers:
         return "lookup error: no working name servers"
+    except dns.resolver.NoAnswer:
+        return "lookup error: no answer"
     ns_names = set()
     for rr in ns_rr:
         if rr.rdtype == dns.rdatatype.NS:
             ns_names.add(rr.target.to_text().lower())
     # see if we can get an AAAA record for one of them
     for ns_name in ns_names:
-        try:
-            ipv6_rr = dns.resolver.query(ns_name, dns.rdatatype.AAAA)
-            for rr in ipv6_rr:
-                if rr.rdtype == dns.rdatatype.AAAA:
-                    return None
-        except dns.resolver.NoAnswer:
-            pass
-        except dns.resolver.NXDOMAIN:
-            pass
+        tries = 0
+        while tries < 3:
+            try:
+                ipv6_rr = dns.resolver.resolve(ns_name, dns.rdatatype.AAAA)
+                for rr in ipv6_rr:
+                    if rr.rdtype == dns.rdatatype.AAAA:
+                        return None
+            except dns.resolver.NoAnswer:
+                break
+            except dns.resolver.NXDOMAIN:
+                break
+            except dns.exception.Timeout:
+                tries += 1
+
+        if tries == 3:
+            return "all AAAA lookups timed out"
+
     # if not, we have no IPv6
     return "no IPv6 nameservers"
 
